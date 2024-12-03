2a9be57968bf370a42365443e586f231ecc60a13
Kyle Harding <kyle@balena.io>
2024-12-03 16:04:15 -0500

    100 percent code coverage
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- test/handlers/deployment-protection-rule.test.ts
+++ test/handlers/deployment-protection-rule.test.ts
@@ -43,7 +43,7 @@ describe('Deployment Protection Rule Handler', () => {
 
 	beforeEach(() => {
 		nock.disableNetConnect();
-		process.env.BYPASS_ACTORS = '5,10';
+		process.env.BYPASS_ACTORS = '5';
 		probot = new Probot({
 			appId: 456,
 			privateKey,
@@ -107,65 +107,74 @@ describe('Deployment Protection Rule Handler', () => {
 		expect(nock.pendingMocks()).toStrictEqual([]);
 	});
 
-	// test('ignores deployment from unauthorized user', async () => {
-	// 	const mock = nock('https://api.github.com')
-	// 		.post('/app/installations/12345678/access_tokens')
-	// 		.reply(200, { token: 'test', permissions: { issues: 'write' } })
-	// 		.get('/app')
-	// 		.reply(200, { id: 456 })
-	// 		.get('/repos/test-org/test-repo/pulls/123/reviews')
-	// 		.reply(200, [{ commit_id: 'test-sha', body: '/deploy please' }]);
-	// 	const payload = {
-	// 		...testFixtures.deployment_protection_rule,
-	// 		deployment: {
-	// 			creator: {
-	// 				login: 'unauthorized-user',
-	// 				id: 789,
-	// 			},
-	// 		},
-	// 	};
+	test('handles undefined BYPASS_ACTORS', async () => {
+		delete process.env.BYPASS_ACTORS;
 
-	// 	await probot.receive({
-	// 		name: 'deployment_protection_rule',
-	// 		payload,
-	// 	});
+		const mock = nock('https://api.github.com')
+			.post('/app/installations/12345678/access_tokens')
+			.reply(200, { token: 'test', permissions: { issues: 'write' } })
+			.get('/app')
+			.reply(200, { id: 456 })
+			.get('/repos/test-org/test-repo/pulls/123/reviews')
+			.reply(200, [])
+			.get('/repos/test-org/test-repo/issues/123/comments')
+			.reply(200, [])
+			.post('/repos/test-org/test-repo/issues/123/comments', {
+				body: instructionalComment,
+			})
+			.reply(200);
 
-	// 	expect(mock.pendingMocks()).toStrictEqual([]);
-	// });
+		await probot.receive({
+			name: 'deployment_protection_rule',
+			payload: testFixtures.deployment_protection_rule,
+		});
 
-	// test('handles undefined BYPASS_ACTORS', async () => {
-	// 	process.env.BYPASS_ACTORS = '';
+		expect(mock.pendingMocks()).toStrictEqual([]);
+	});
 
-	// 	const mock = nock('https://api.github.com')
-	// 		.post('/app/installations/12345678/access_tokens')
-	// 		.reply(200, { token: 'test', permissions: { issues: 'write' } })
-	// 		.get('/repos/test-org/test-repo/pulls/123/reviews')
-	// 		.reply(200, []);
+	test('handles empty BYPASS_ACTORS', async () => {
+		process.env.BYPASS_ACTORS = '';
 
-	// 	await probot.receive({
-	// 		name: 'deployment_protection_rule',
-	// 		payload: testFixtures.deployment_protection_rule,
-	// 	});
+		const mock = nock('https://api.github.com')
+			.post('/app/installations/12345678/access_tokens')
+			.reply(200, { token: 'test', permissions: { issues: 'write' } })
+			.get('/app')
+			.reply(200, { id: 456 })
+			.get('/repos/test-org/test-repo/pulls/123/reviews')
+			.reply(200, [])
+			.get('/repos/test-org/test-repo/issues/123/comments')
+			.reply(200, [])
+			.post('/repos/test-org/test-repo/issues/123/comments', {
+				body: instructionalComment,
+			})
+			.reply(200);
 
-	// 	expect(mock.pendingMocks()).toStrictEqual([]);
-	// });
+		await probot.receive({
+			name: 'deployment_protection_rule',
+			payload: testFixtures.deployment_protection_rule,
+		});
 
-	// test('handles defined bypass actors with multiple values', async () => {
-	// 	process.env.BYPASS_ACTORS = '5,10,15';
+		expect(mock.pendingMocks()).toStrictEqual([]);
+	});
 
-	// 	const mock = nock('https://api.github.com')
-	// 		.post('/app/installations/12345678/access_tokens')
-	// 		.reply(200, { token: 'test', permissions: { issues: 'write' } })
-	// 		.get('/repos/test-org/test-repo/pulls/123/reviews')
-	// 		.reply(200, []);
+	test('handles multiple BYPASS_ACTORS values', async () => {
+		process.env.BYPASS_ACTORS = '5,10,15';
 
-	// 	await probot.receive({
-	// 		name: 'deployment_protection_rule',
-	// 		payload: testFixtures.deployment_protection_rule,
-	// 	});
+		const mock = nock('https://api.github.com')
+			.post('/app/installations/12345678/access_tokens')
+			.reply(200, { token: 'test', permissions: { issues: 'write' } })
+			.post(
+				'/repos/test-org/test-repo/actions/runs/1234/deployment_protection_rule',
+			)
+			.reply(200);
 
-	// 	expect(mock.pendingMocks()).toStrictEqual([]);
-	// });
+		await probot.receive({
+			name: 'deployment_protection_rule',
+			payload: testFixtures.deployment_protection_rule,
+		});
+
+		expect(mock.pendingMocks()).toStrictEqual([]);
+	});
 
 	test('approves deployment for APPROVED pull request review', async () => {
 		const mock = nock('https://api.github.com')
